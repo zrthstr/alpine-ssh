f85874986d1197c15489067a85ee8642e74baecb
polandj <polandj@garble.org>
2016-02-03 12:26:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+
+if [ "${AUTHORIZED_KEYS}x" = "x" ]; then
+  echo "ERROR: You need to supply AUTHORIZED_KEYS environment variable!"
+  exit 1
+fi
+
+mkdir -p /root/.ssh/
+echo ${AUTHORIZED_KEYS} > /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/authorized_keys
 
 rc-status
 touch /run/openrc/softlevel
